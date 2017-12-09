7f5b103ea052d7cf73e2120fb49f8d5841a86af9
root <root@ip-172-31-5-39.ap-southeast-2.compute.internal>
2017-12-09 14:06:27 +0000

    added compilation in amazon g2.x8large

---
+++ builds/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/monero/xmr-stak/xmrstak/backend/backendConnector.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/backendConnector.cpp.o"
+  "/home/monero/xmr-stak/xmrstak/backend/cpu/crypto/cryptonight_common.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/crypto/cryptonight_common.cpp.o"
+  "/home/monero/xmr-stak/xmrstak/backend/cpu/hwlocMemory.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/hwlocMemory.cpp.o"
+  "/home/monero/xmr-stak/xmrstak/backend/cpu/jconf.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/jconf.cpp.o"
+  "/home/monero/xmr-stak/xmrstak/backend/cpu/minethd.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/minethd.cpp.o"
+  "/home/monero/xmr-stak/xmrstak/backend/globalStates.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/globalStates.cpp.o"
+  "/home/monero/xmr-stak/xmrstak/http/httpd.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/http/httpd.cpp.o"
+  "/home/monero/xmr-stak/xmrstak/http/webdesign.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/http/webdesign.cpp.o"
+  "/home/monero/xmr-stak/xmrstak/jconf.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/jconf.cpp.o"
+  "/home/monero/xmr-stak/xmrstak/misc/console.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/console.cpp.o"
+  "/home/monero/xmr-stak/xmrstak/misc/executor.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/executor.cpp.o"
+  "/home/monero/xmr-stak/xmrstak/misc/telemetry.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/telemetry.cpp.o"
+  "/home/monero/xmr-stak/xmrstak/misc/utility.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/utility.cpp.o"
+  "/home/monero/xmr-stak/xmrstak/net/jpsock.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/net/jpsock.cpp.o"
+  "/home/monero/xmr-stak/xmrstak/net/socket.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/net/socket.cpp.o"
+  "/home/monero/xmr-stak/xmrstak/version.cpp" "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-backend.dir/xmrstak/version.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BACKEND_TYPE=nvidia-cpu"
+  "CONF_NO_HWLOC"
+  "CONF_NO_OPENCL"
+  "GIT_BRANCH=master"
+  "GIT_COMMIT_HASH=30888a3"
+  "XMRSTAK_CUDA_ARCH_LIST=30+35+37+50+52+60+61+62+70"
+  "XMR_STAK_LARGEGRID=ON"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/monero/xmr-stak/."
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/monero/build-x86_64-linux-gnu/CMakeFiles/xmr-stak-c.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
